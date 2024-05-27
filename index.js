47be2c7cbde111374c025499bca2fdc0b4b8c211
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-05-26 22:58:02 -0500

    Rearrange logic to shorten code and address input error

--- index.js
+++ index.js
@@ -2,100 +2,62 @@ import sharp from 'sharp';
 import fs from 'fs';
 import readline from 'readline';
 import { Readable } from 'stream';
-// Ask the user for a URL.
-// Ask the user for a filename.
-// Download the image from the URL.
-// Save the image to the filename in .cache/{filename}.
-// Convert the image to black and white.
-// Save the black and white image to the filename in ./{filename}.
-// Done.
-function promptText(item) {
-    return new Promise((resolve, reject) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
 
-        rl.question(`${item.message} `, (answer) => {
-            rl.close();
-            resolve(answer);
-        });
-    });
-}
+const options = {};
 
-function process(filename) {
-    const image = sharp(`.cache/${filename}`, {
-        raw: { width, height, channels: 4 }
-    });
-    image
-        .greyscale()
-        .toFile(filename, (err, info) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Done.');
-            }
-        });
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-async function main() {
-    let url = await promptText({ message: 'Enter a URL:' });
-    if (!url) {
-        console.log('No URL provided. Using default URL.');
-        url = 'https://pkmncards.com/wp-content/uploads/clc_en_003-charizard.png';
+rl.question('Enter a filename: ', (filename) => {
+    function process(filename) {
+        const image = sharp(`.cache/${filename}`, options);
+        image
+            .greyscale()
+            .toFile(filename, (err, info) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Done.');
+                }
+            });
     }
-    let filename = await promptText({ message: 'Enter a filename:' });
     if (!filename) {
-        console.log('No filename provided. Using default filename.');
         filename = 'charizard.png';
+        console.log(`No filename provided. Using ${filename}.`);
     }
-    const IMAGE_PATH = `.cache/${filename}`;
-    if (!fs.existsSync('.cache')) {
-        fs.mkdirSync('.cache');
+    if (fs.existsSync(`images/${filename}`)) {
+        rl.close();
+        process(filename);
+        console.log(`File processed: images/${filename}`);
+        return;
     }
-    return fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log(response);
-            const contentType = response.headers.get('content-type');
-            if (!contentType.startsWith('image')) {
+    rl.question('Enter a URL: ', (url) => {
+        rl.close();
+        if (!url) {
+            url = 'https://pkmncards.com/wp-content/uploads/clc_en_003-charizard.png';
+            console.log(`No URL provided. Using ${url}.`);
+        }
+        if (!fs.existsSync('.cache')) {
+            fs.mkdirSync('.cache');
+        }
+        if (!fs.existsSync('images')) {
+            fs.mkdirSync('images');
+        }
+        fetch(url).then((response) => {
+            if (!response.headers.get('content-type').startsWith('image')) {
                 throw new Error('Content type is not an image');
             }
-            const contentLength = response.headers.get('content-length');
-            if (contentLength > 1_000_000_000) {
+            if (1_000_000_000 < response.headers.get('content-length')) {
                 throw new Error('Content length is too large');
             }
-            return response;
-        }).then((response) => {
-            const fileStream = fs.createWriteStream(IMAGE_PATH, { flags: 'w' })
+            const fileStream = fs.createWriteStream(`.cache/${filename}`);
             fileStream.on('finish', () => {
                 console.log('Downloaded.');
-                const width = 800;
-                const height = 800;
-                const image = sharp(IMAGE_PATH, { raw: { width, height, channels: 4 } });
-                image
-                    .greyscale()
-                    .toFile(filename, (err, info) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            console.log('Done.');
-                        }
-                    });
-
-            });
-            fileStream.on('error', (error) => {
-                console.error('There has been a problem with your writable stream:', error);
-            });
-            fileStream.on('close', () => {
-                console.log('Writable stream closed.');
+                process(filename);
             });
             Readable.fromWeb(response.body).pipe(fileStream);
-        }).catch((error) => {
-            console.error('There has been a problem with your fetch operation:', error);
         });
-}
-
-main();
+    });
+});
