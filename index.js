7b4c86fb7edead713dd13def56353b22461f8d93
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-05-29 16:53:17 -0500

    Modify regions for default card

--- index.js
+++ index.js
@@ -8,14 +8,25 @@ if (process.argv.includes('--no-interaction')) {
         src: '.cache/charizard.png',
         outdir: 'images',
         regions: [
-            { x: 63, y: 85, width: 93, height: 78 },
+            { x: 0, y: 0, width: 735, height: 39 }, // top card edge.
+            { x: 0, y: 986, width: 735, height: 39 }, // bottom card edge.
+            { x: 0, y: 0, width: 33, height: 1025 }, // left card edge.
+            { x: 702, y: 0, width: 33, height: 1025 }, // right card edge.
+            { x: 63, y: 60, width: 93, height: 25 }, // previous stage.
+            { x: 38, y: 85, width: 25, height: 78 }, // previous stage.
+            { x: 156, y: 85, width: 25, height: 78 }, // previous stage.
+            { x: 63, y: 85, width: 93, height: 78 }, // previous stage.
+            { x: 63, y: 163, width: 93, height: 25 }, // previous stage.
             { x: 74, y: 118, width: 585, height: 421 }, // artwork.
-            { filter: 'dotted', x: 191, y: 70, width: 420, height: 50 },
-            { filter: 'dotted', x: 150, y: 740, width: 515, height: 80 },
-            { filter: 'dotted', x: 70, y: 842, width: 630, height: 20 },
+            { filter: 'dotted', x: 191, y: 70, width: 420, height: 50 }, // name and HP.
+            { filter: 'dotted', x: 150, y: 740, width: 515, height: 80 }, // second attack.
+            { filter: 'dotted', x: 70, y: 842, width: 630, height: 20 }, // weakness, resistance, and retreat cost.
+            { filter: 'dotted', x: 0, y: 552, width: 720, height: 174 }, // power name.
             { filter: 'darkest', x: 162, y: 46, width: 558, height: 20 }, /// evolution details.
             { filter: 'darkest', x: 75, y: 67, width: 70, height: 16 }, // stage > 1.
-            { filter: 'darkest', x: 0, y: 552, width: 720, height: 174 }, // stats, power, etc.
+            { filter: 'darkest', x: 0, y: 552, width: 720, height: 40 }, // stats, power, etc.
+            { filter: 'darkest', x: 470, y: 600, width: 190, height: 28 }, // stats, power, etc.
+            { filter: 'darkest', x: 0, y: 628, width: 720, height: 96 }, // stats, power, etc.
             { filter: 'darkest', x: 615, y: 65, width: 50, height: 55 }, // type.
             { filter: 'darkest', x: 50, y: 735, width: 90, height: 90 }, // second attack energy.
             { filter: 'darkest', x: 50, y: 832, width: 640, height: 5 }, // bottom line.
